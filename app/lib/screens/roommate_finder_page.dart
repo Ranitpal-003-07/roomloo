663adc447e05650adb09385ddc6ea51fc1156e69
Ranitpal-003-07 <ranitpal699@gmail.com>
2025-02-23 21:52:54 +0530

    Updated Files

---
+++ app/lib/screens/roommate_finder_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RoommateFinderPage extends StatelessWidget {
+  const RoommateFinderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Find Roommates"),
+    );
+  }
+}
